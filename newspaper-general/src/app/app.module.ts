fbdcbd7e1fa2b5a6c80e3504cc50c6b051ea788d
MaximeLombard <112693934+MaximeLombard@users.noreply.github.com>
2023-07-06 16:58:49 +0200

    ALV

--- newspaper-general/src/app/app.module.ts
+++ newspaper-general/src/app/app.module.ts
@@ -3,10 +3,14 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HomepageComponent } from './homepage/homepage.component';
+import { ArticleCardComponent } from './shared/article-card/article-card.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomepageComponent,
+    ArticleCardComponent
   ],
   imports: [
     BrowserModule,
