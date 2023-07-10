a388c62f1be4e34651275319443c9420882f3b05
Jeremy Bussy <bussyjeremy@ekolis-eu.com>
2023-07-10 15:42:24 +0200

    add route and check post work in progress

--- newspaper-general/src/app/app-routing.module.ts
+++ newspaper-general/src/app/app-routing.module.ts
@@ -1,9 +1,18 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { LoginComponent } from './auth/login/login.component';
+import { RoleGuard } from './auth/role.guard';
+import { BackofficeComponent } from './backoffice/backoffice.component';
 import { HomepageComponent } from './homepage/homepage.component';
 
 const routes: Routes = [
-  { path: '', component: HomepageComponent }
+  { path: '', component: HomepageComponent },
+  { path: 'login', component: LoginComponent},
+  {
+    path: 'create',
+    component: BackofficeComponent,
+    canActivate: [RoleGuard] // Apply the RoleGuard to the create page
+  }
 ];
 
 @NgModule({
