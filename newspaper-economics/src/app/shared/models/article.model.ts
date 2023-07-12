46474da4a868b3544183be8412f08da15633de66
NatachaEt <natacha.ethievant@unsolite.com>
2023-07-12 16:09:55 +0200

    Update article.model.ts

--- newspaper-economics/src/app/shared/models/article.model.ts
+++ newspaper-economics/src/app/shared/models/article.model.ts
@@ -5,9 +5,12 @@ export class ArticleModel {
   preview: string | null = null;
   corp: string | null = null;
   image: string | null = null;
+  auteur_id : number | null = null;
   auteur: string | null = null;
   premium: boolean = false;
   visible: boolean = false;
+
+  categorie_id: number | null = null;
   categorie: string | null = null;
   date_creation: string | null = null;
   localisation: LocalisationModel[] = [];
